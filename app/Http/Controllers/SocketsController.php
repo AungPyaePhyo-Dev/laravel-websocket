8a15ee7fcf23fde8261986bfb95c567ba9c1bb1f
AungPyaePhyo-Dev <aungphyo.kh26@gmail.com>
2024-01-25 13:55:47 +0630

    Create Websocket Connect API

---
+++ app/Http/Controllers/SocketsController.php
@@ -0,0 +1,25 @@
+<?php
+
+declare(strict_types=1);
+namespace App\Http\Controllers;
+
+use Illuminate\Broadcasting\Broadcasters\PusherBroadcaster;
+use Illuminate\Http\Request;
+use Pusher\Pusher;
+
+class SocketsController 
+{
+    public function connect(Request $request)
+    {
+        $broadcaster = new PusherBroadcaster(
+            new Pusher(
+                env("PUSHER_APP_KEY"),
+                env("PUSHER_APP_ID"),
+                env("PUSHER_APP_SECRET"),
+                []
+            ) 
+        );
+
+        return $broadcaster->validAuthenticationResponse($request, []);
+    }
+}
